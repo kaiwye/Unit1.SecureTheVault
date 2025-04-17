cf7b71eef81e1969f0a9c36d4fbf884847ea6088
Kai Wui Ye <kye5595@gmail.com>
2025-04-17 14:25:14 -0400

    Value is Secured

---
+++ index.js
@@ -0,0 +1,19 @@
+let code1 = 7 + 3;
+let code2 = 123 / 3 - 1;
+let code3 = (38 % 39) + 1;
+
+let message = "The vault has been secured. The combination is: ";
+
+let codeA = code1 + "-" + code2 + "-" + code3;
+
+console.log(codeA);
+
+let codeB = `${code1}-${code2}-${code3}`;
+
+console.log(codeB);
+
+console.log(message);
+
+console.log(message + codeA);
+
+console.log(message + codeA);
